7da865108de0e0ea659127b2b5b3a0dab3287890
md-taha-ahmed <md.taha.ahmed.projects@gmail.com>
2022-09-21 17:52:03 +0530

    edit: the dataType of subject name

--- SQL/published.sql
+++ SQL/published.sql
@@ -1,253 +1,181 @@
 -- phpMyAdmin SQL Dump
-
 -- version 5.2.0
-
 -- https://www.phpmyadmin.net/
-
 --
-
--- Host: 127.0.0.1
-
--- Generation Time: Sep 17, 2022 at 03:53 PM
-
+-- Host: localhost
+-- Generation Time: Sep 21, 2022 at 02:19 PM
 -- Server version: 10.4.24-MariaDB
-
--- PHP Version: 7.4.29
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-
 START TRANSACTION;
-
 SET time_zone = "+00:00";
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
 
-;
-
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
-
-;
-
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
-
-;
-
-/*!40101 SET NAMES utf8mb4 */
-
-;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
-
 -- Database: `published`
-
 --
 
 -- --------------------------------------------------------
 
 --
-
 -- Table structure for table `cat`
-
 --
 
-CREATE TABLE
-    `cat` (
-        `id` int(4) NOT NULL,
-        `cat_name` varchar(30) NOT NULL,
-        `cat_desc` text NOT NULL
-    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+CREATE TABLE `cat` (
+  `id` int(4) NOT NULL,
+  `cat_name` varchar(30) NOT NULL,
+  `cat_logo` varchar(50) NOT NULL,
+  `cat_desc` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
-
 -- Dumping data for table `cat`
-
 --
 
-INSERT INTO
-    `cat` (`id`, `cat_name`, `cat_desc`)
-VALUES (
-        1,
-        'Computer Science/IT',
-        'Various resources related to Computer Science and Information technology field.'
-    ), (
-        2,
-        'Mechanical Engineering',
-        'Various resources related to Mechanical Engineering field, organized and well maintained.'
-    );
+INSERT INTO `cat` (`id`, `cat_name`, `cat_logo`, `cat_desc`) VALUES
+(2, 'IS', '.', '.');
 
 -- --------------------------------------------------------
 
 --
-
 -- Table structure for table `doc`
-
 --
 
-CREATE TABLE
-    `doc` (
-        `id` int(11) NOT NULL,
-        `sub-id` int(11) NOT NULL,
-        `doc-name` varchar(50) NOT NULL,
-        `doc-desc` text NOT NULL
-    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+CREATE TABLE `doc` (
+  `id` int(11) NOT NULL,
+  `sub-id` int(11) NOT NULL,
+  `doc-name` varchar(50) NOT NULL,
+  `doc-desc` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
-
 -- Table structure for table `sem`
-
 --
 
-CREATE TABLE
-    `sem` (
-        `id` int(11) NOT NULL,
-        `cat-id` int(11) NOT NULL,
-        `sem-num` int(11) NOT NULL
-    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+CREATE TABLE `sem` (
+  `id` int(11) NOT NULL,
+  `sem-num` int(11) NOT NULL,
+  `cat_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sem`
+--
+
+INSERT INTO `sem` (`id`, `sem-num`, `cat_id`) VALUES
+(3, 2, 2),
+(4, 2, 2);
 
 -- --------------------------------------------------------
 
 --
-
 -- Table structure for table `sub`
-
 --
 
-CREATE TABLE
-    `sub` (
-        `id` int(11) NOT NULL,
-        `sem-id` int(11) NOT NULL,
-        `sub-name` int(100) NOT NULL,
-        `sub-code` varchar(10) NOT NULL
-    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+CREATE TABLE `sub` (
+  `id` int(11) NOT NULL,
+  `sem-id` int(11) NOT NULL,
+  `sub-name` varchar(100) NOT NULL,
+  `sub-code` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `sub`
+--
 
+INSERT INTO `sub` (`id`, `sem-id`, `sub-name`, `sub-code`) VALUES
+(1, 3, 'java', '18is53');
+
+--
 -- Indexes for dumped tables
-
 --
 
 --
-
 -- Indexes for table `cat`
+--
+ALTER TABLE `cat`
+  ADD PRIMARY KEY (`id`);
 
 --
-
-ALTER TABLE `cat` ADD PRIMARY KEY (`id`);
-
---
-
 -- Indexes for table `doc`
-
 --
-
 ALTER TABLE `doc`
-ADD PRIMARY KEY (`id`),
-ADD
-    UNIQUE KEY `sub-id` (`sub-id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `sub-id` (`sub-id`);
 
 --
-
 -- Indexes for table `sem`
-
 --
-
 ALTER TABLE `sem`
-ADD
-    PRIMARY KEY (`id`),
-    --
-    -- Indexes for table `sub`
-    --
-ALTER TABLE `sub`
-ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `cat_id` (`cat_id`);
 
 --
+-- Indexes for table `sub`
+--
+ALTER TABLE `sub`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `sem-id` (`sem-id`);
 
+--
 -- AUTO_INCREMENT for dumped tables
-
 --
 
 --
-
 -- AUTO_INCREMENT for table `cat`
+--
+ALTER TABLE `cat`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
-
-ALTER TABLE
-    `cat` MODIFY `id` int(4) NOT NULL AUTO_INCREMENT,
-    AUTO_INCREMENT = 3;
-
---
-
 -- AUTO_INCREMENT for table `doc`
-
 --
-
-ALTER TABLE `doc` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
-
--- AUTO_INCREMENT for table `sem`
-
---
-
-ALTER TABLE `sem` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
-
--- AUTO_INCREMENT for table `sub`
-
---
-
-ALTER TABLE `sub` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
-
--- Constraints for dumped tables
-
---
-
---
-
--- Constraints for table `doc`
-
---
-
 ALTER TABLE `doc`
-ADD
-    CONSTRAINT `doc_ibfk_1` FOREIGN KEY (`sub-id`) REFERENCES `sub` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
-
--- Constraints for table `sem`
-
+-- AUTO_INCREMENT for table `sem`
 --
-
 ALTER TABLE `sem`
-ADD
-    CONSTRAINT `sem_ibfk_1` FOREIGN KEY (`cat-id`) REFERENCES `cat` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
-
--- Constraints for table `sub`
-
+-- AUTO_INCREMENT for table `sub`
 --
-
 ALTER TABLE `sub`
-ADD
-    CONSTRAINT `sub_ibfk_1` FOREIGN KEY (`sem-id`) REFERENCES `sem` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `doc`
+--
+ALTER TABLE `doc`
+  ADD CONSTRAINT `doc_ibfk_1` FOREIGN KEY (`sub-id`) REFERENCES `sub` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `sem`
+--
+ALTER TABLE `sem`
+  ADD CONSTRAINT `test` FOREIGN KEY (`cat_id`) REFERENCES `cat` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `sub`
+--
+ALTER TABLE `sub`
+  ADD CONSTRAINT `sub_ibfk_1` FOREIGN KEY (`sem-id`) REFERENCES `sem` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
-
-;
-
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
-
-;
-
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
-
-;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
