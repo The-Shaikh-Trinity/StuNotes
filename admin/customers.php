ca0ea210491e0971288a8e23a60b227c5a4f72e6
md-taha-ahmed <md.taha.ahmed.projects@gmail.com>
2022-09-21 17:27:59 +0530

    modify: customer page

--- admin/customers.php
+++ admin/customers.php
@@ -7,14 +7,11 @@ include "includes/head.php";
     include "includes/header.php"
     ?>
 
-
     <?php
     include "includes/sidebar.php";
     ?>
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-        <?php
-        message();
-        ?>
+
         <div class="container">
             <div class="row align-items-start">
                 <div class="col">
@@ -34,50 +31,40 @@ include "includes/head.php";
                 </div>
             </div>
         </div>
-        <?php
 
-        if (isset($_GET['edit'])) {
-            $_SESSION['id'] = $_GET['edit'];
-            $data = get_user($_SESSION['id']);
-
-        ?>
+        <br>
+        <h2>Edit Customer Details</h2>
+        <form action="customers.php" method="POST">
+            <div class="form-group">
+                <label>First name</label>
+                <input pattern="[A-Za-z_]{1,15}" type="text" class="form-control" placeholder="user_fname" name="fname">
+                <div class="form-text">please enter the first name in range(1-30) character/s , special character & numbers not allowed !</div>
+            </div>
             <br>
-            <h2>Edit Customer Details</h2>
-            <form action="customers.php" method="POST">
-                <div class="form-group">
-                    <label>First name</label>
-                    <input pattern="[A-Za-z_]{1,15}" type="text" class="form-control" placeholder="<?php echo $data[0]['user_fname'] ?>" name="fname">
-                    <div class="form-text">please enter the first name in range(1-30) character/s , special character & numbers not allowed !</div>
-                </div>
-                <br>
-                <div class="form-group">
-                    <label for="validationTooltip01">Last name</label>
-                    <input pattern="[A-Za-z_]{1,15}" id="validationTooltip01" type="text" class="form-control" placeholder="<?php echo $data[0]['user_lname'] ?>" name="lname">
-                    <div class="form-text">please enter the last name in range(1-30) character/s , special character & numbers not allowed !</div>
-                </div>
-                <br>
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="<?php echo $data[0]['email'] ?>" name="email">
-                    <div class="form-text">please enter the email in format : example@gmail.com.</div>
-                </div>
-                <br>
-                <div class="form-group">
-                    <label for="inputAddress2">Address</label>
-                    <input pattern="^[#.0-9a-zA-Z\s,-]+$" type="text" class="form-control" id="inputAddress2" placeholder="<?php echo $data[0]['user_address'] ?>" name="address">
-                </div>
-                <div class="form-text">please enter the email in format : #1, North Street, Chennai - 11.</div>
-                <br>
-                <button type="submit" class="btn btn-outline-success" value="update" name="update">Submit</button>
-                <button type=" submit" class="btn btn-outline-danger" value="cancel" name="cancel">Cancel</button>
-                <br> <br>
-            </form>
+            <div class="form-group">
+                <label for="validationTooltip01">Last name</label>
+                <input pattern="[A-Za-z_]{1,15}" id="validationTooltip01" type="text" class="form-control" placeholder="user_lname" name="lname">
+                <div class="form-text">please enter the last name in range(1-30) character/s , special character & numbers not allowed !</div>
+            </div>
+            <br>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="email" name="email">
+                <div class="form-text">please enter the email in format : example@gmail.com.</div>
+            </div>
+            <br>
+            <div class="form-group">
+                <label for="inputAddress2">Address</label>
+                <input pattern="^[#.0-9a-zA-Z\s,-]+$" type="text" class="form-control" id="inputAddress2" placeholder="user_address" name="address">
+            </div>
+            <div class="form-text">please enter the email in format : #1, North Street, Chennai - 11.</div>
+            <br>
+            <button type="submit" class="btn btn-outline-success" value="update" name="update">Submit</button>
+            <button type=" submit" class="btn btn-outline-danger" value="cancel" name="cancel">Cancel</button>
+            <br> <br>
+        </form>
 
-        <?php
-        }
-        edit_user($_SESSION['id']);
 
-        ?>
         <div class="table-responsive">
             <table class="table table-striped table-sm">
                 <thead>
@@ -91,36 +78,21 @@ include "includes/head.php";
                 </thead>
 
                 <tbody>
-                    <?php
-                    $data = all_users();
-                    delete_user();
-                    if (isset($_GET['search_user'])) {
-                        $query = search_user();
-                        if (isset($query)) {
-                            $data = $query;
-                        } else {
-                            get_redirect("customers.php");
-                        }
-                    } elseif (isset($_GET['id'])) {
-                        $data = get_user_details();
-                    }
-                    $num = sizeof($data);
-                    for ($i = 0; $i < $num; $i++) {
-                    ?>
-                        <tr>
-                            <td><?php echo $data[$i]['user_id'] ?></td>
-                            <td><?php echo $data[$i]['user_fname'] ?></td>
-                            <td><?php echo $data[$i]['user_lname'] ?></td>
-                            <td><?php echo $data[$i]['email'] ?></td>
-                            <td><?php echo $data[$i]['user_address'] ?></td>
-                            <td>
-                                <button type="button" class="btn pull-left btn-outline-warning"><a style="text-decoration: none; color:black;" href="customers.php?edit=<?php echo $data[$i]['user_id'] ?>">Edit</a></button>
-                            </td>
-                            <td>
-                                <button type="button" class="btn pull-left btn-outline-danger"><a style="text-decoration: none; color:black;" href="customers.php?delete=<?php echo $data[$i]['user_id'] ?>">Delete</a></button>
-                            </td>
-                        </tr>
-                    <?php  }
+
+                    <tr>
+                        <td>user_id</td>
+                        <td>user_fname</td>
+                        <td>user_lname</td>
+                        <td>email</td>
+                        <td>user_address</td>
+                        <td>
+                            <button type="button" class="btn pull-left btn-outline-warning"><a style="text-decoration: none; color:black;" href="customers.php?edit=user_id">Edit</a></button>
+                        </td>
+                        <td>
+                            <button type="button" class="btn pull-left btn-outline-danger"><a style="text-decoration: none; color:black;" href="customers.php?delete=user_id">Delete</a></button>
+                        </td>
+                    </tr>
+
                     ?>
                 </tbody>
             </table>
