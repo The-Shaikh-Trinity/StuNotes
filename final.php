92a2dd38802c7a23b76fd236875d85266e800977
kazimayaan <kazimayaan486@gmail.com>
2022-10-09 18:52:46 +0530

    breadcrumb change

--- final.php
+++ final.php
@@ -93,12 +93,28 @@ include "includes/head.php"
               <a href="#" class="ml-1 text-sm font-medium text-gray-700 hover:text-indigo-500 md:ml-2 dark:text-gray-400 ">Published Notes</a>
             </div>
           </li>
+          <li>
+            <div class="flex items-center">
+              <svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
+              </svg>
+              <a href="#" class="ml-1 text-sm font-medium text-gray-700 hover:text-indigo-500 md:ml-2 dark:text-gray-400 ">Category</a>
+            </div>
+          </li>
+          <li>
+            <div class="flex items-center">
+              <svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
+              </svg>
+              <a href="#" class="ml-1 text-sm font-medium text-gray-700 hover:text-indigo-500 md:ml-2 dark:text-gray-400 ">Semester</a>
+            </div>
+          </li>
           <li aria-current="page">
             <div class="flex items-center">
               <svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
               </svg>
-              <span class="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">Category</span>
+              <span class="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">Subject</span>
             </div>
           </li>
         </ol>
@@ -108,6 +124,9 @@ include "includes/head.php"
   </div>
   <div class="final content">
 
+
+
+  
   </div>
 
 
